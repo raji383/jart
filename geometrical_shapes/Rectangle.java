24fd161a62acc15989cd1d71a48701cb8c416ca1
aotchoun <aotchoun23@gmail.com>
2026-02-27 15:40:27 +0000

    rectangle done

--- geometrical_shapes/Rectangle.java
+++ geometrical_shapes/Rectangle.java
@@ -1,6 +1,55 @@
 package geometrical_shapes;
 
-public class Rectangle {
+import java.awt.Color;
+import java.util.Random;
 
+public class Rectangle implements Drawable {
     
+    private Point topLeft;
+    private Point bottomRight;
+    private Color color;
+
+    public Rectangle(Point topLeft, Point bottomRight) {
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+        this.color = getColor();
+    }
+
+    @Override
+    public void draw(Displayable displayable) {
+        int x1 = topLeft.x;
+        int y1 = topLeft.y;
+        int x2 = bottomRight.x;
+        int y2 = bottomRight.y;
+
+        // Côté haut : (x1, y1) -> (x2, y1)
+        for (int x = x1; x <= x2; x++) {
+            displayable.display(x, y1, color);
+        }
+
+        // Côté bas : (x1, y2) -> (x2, y2)
+        for (int x = x1; x <= x2; x++) {
+            displayable.display(x, y2, color);
+        }
+
+        // Côté gauche : (x1, y1) -> (x1, y2)
+        for (int y = y1; y <= y2; y++) {
+            displayable.display(x1, y, color);
+        }
+
+        // Côté droit : (x2, y1) -> (x2, y2)
+        for (int y = y1; y <= y2; y++) {
+            displayable.display(x2, y, color);
+        }
+    }
+
+    @Override
+    public Color getColor() {
+        Random rand = new Random();
+
+        int r = rand.nextInt(256);
+        int g = rand.nextInt(256);
+        int b = rand.nextInt(256);
+        return new Color(r, g, b);
+    }
 }
